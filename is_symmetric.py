f4341d159c8e030792138fc5de71061359f1d3e3
Cayden Lau <suanoo5196@gmail.com>
2020-08-19 23:18:09 +0000

    Used user input instead of inputting the values in the code

--- is_symmetric.py
+++ is_symmetric.py
@@ -1,13 +1,14 @@
+input_string = input('')
+reverse_string = input_string[::-1]
+
 def is_symmetric(input_string): 
-  reverse = input_string[::-1]
   if reverse_string == input_string:
     print("True")
   else:
     print("False") 
-
 is_symmetric('racecar')
 is_symmetric('batman')
 
-print('testing is_symmetric on input racecar and batman')
+print('testing is_symmetric on the input {}'.format(input_string))
 assert reverse_string == input_string, 'The input_string {} is not a palindrome'.format(input_string)
 print('PASSED')
