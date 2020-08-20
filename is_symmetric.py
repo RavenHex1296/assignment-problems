81ecf3079359b82c7d83f6a3c8fbcf83d9c577f6
Cayden Lau <suanoo5196@gmail.com>
2020-08-20 01:32:24 +0000

    indented

--- is_symmetric.py
+++ is_symmetric.py
@@ -6,8 +6,6 @@ def is_symmetric(input_string):
     print("True")
   else:
     print("False") 
-is_symmetric('racecar')
-is_symmetric('batman')
 
 print('testing is_symmetric on the input {}'.format(input_string))
 assert reverse_string == input_string, 'The input_string {} is not a palindrome'.format(input_string)
