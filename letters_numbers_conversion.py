cd7865983ca983984f39329d9268162d165ca8e6
Cayden Lau <suanoo5196@gmail.com>
2020-08-20 05:06:04 +0000

    final code

--- letters_numbers_conversion.py
+++ letters_numbers_conversion.py
@@ -1,4 +1,6 @@
-letters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letters = ['a', 'b', 'c', 'd', 'e',
+           'f', 'g', 'h', 'i', 'j',
+           ...]
 
 
 def convert_to_numbers(input_string):
