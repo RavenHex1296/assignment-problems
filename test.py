37a35ce633eee5819c6b5319f7702ee3d5811aaf
Cayden Lau <suanoo5196@gmail.com>
2020-08-20 04:28:10 +0000

    2 a cats

--- test.py
+++ test.py
@@ -1,29 +1,15 @@
 letters = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-def convert_to_numbers(input_string):
-    input_numbers = list(input_string)
-    converted_letters = []
-
-    for letter in input_numbers:
-        converted_letters.append(letters.index(letter))
-
-    return converted_letters
-
-print(convert_to_numbers("a cat"))
-
-print("Testing convert_to_numbers for input 'a cat'")
-assert convert_to_numbers("a cat") == [1, 0, 3, 1, 20], "Wrong conversion"
-print("PASSED")
-
-
 def convert_to_letters(input_string):
+    print('a')
     converted_numbers = ""
     for number in input_string:
         converted_numbers += letters[number]
+    print('b')
 
     print(converted_numbers)
     return converted_numbers
+    print('c')
 
 convert_to_letters([1, 0, 3, 1, 20])
 
