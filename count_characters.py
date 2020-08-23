870b6e36fcdc2ed853dcc4bad6c23f184e04f9fa
Cayden Lau <suanoo5196@gmail.com>
2020-08-23 23:44:54 +0000

    final version of draft 1

---
+++ count_characters.py
@@ -0,0 +1,14 @@
+def count_characters(input_string):
+    final_dictionary = {}
+    lowercase = list(input_string.lower())
+
+    for character in lowercase:
+        final_dictionary[character] = lowercase.count(character)
+
+    return final_dictionary
+
+print(count_characters('A cat!!!'))
+
+print("Asserting count_characters works on input 'A cat!!!'")
+assert count_characters("A cat"), "Code is busted"
+print("PASSED")
