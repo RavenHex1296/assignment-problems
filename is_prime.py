3c3963b465a50f02d7c8df7756bb6061e5431e38
Cayden Lau <suanoo5196@gmail.com>
2020-08-23 16:03:37 +0000

    added what the function is checking

--- is_prime.py
+++ is_prime.py
@@ -5,12 +5,13 @@ def is_prime(n):
             return False
     return True
 
+print("Checking if 59 is a prime number")
 print(is_prime(59))
 print("Testing is_prime on input 59")
 assert is_prime(59), "59 is prime"
 print("PASSED")
 
-
+print("Checking if 51 is a prime number")
 print(is_prime(51))
 print("Testing is_prime on input 51")
 assert not is_prime(51), "51 isn't prime"
