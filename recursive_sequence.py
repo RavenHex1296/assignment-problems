c4523c60620dd39459563ed24442f96d7400973a
Cayden Lau <suanoo5196@gmail.com>
2020-08-24 19:07:36 +0000

    first draft of 2-3

--- recursive_sequence.py
+++ recursive_sequence.py
@@ -1,2 +1,22 @@
-def first_n_terms:
-    
+def first_n_terms(n):
+    assert n > 0, "Input must be positive"
+    sequence = []
+    for element in range(1, n):
+        if element == 1:
+            preceding_term = 5
+            sequence.append(5)
+
+        sequence.append(3 * preceding_term - 4)
+        preceding_term = 3 * preceding_term - 4
+
+    return sequence
+
+print(first_n_terms(10))
+
+
+def nth_term(n):
+    if n == 1:
+        return 5
+    return 3 * nth_term(n - 1) - 4
+
+print(nth_term(10))
