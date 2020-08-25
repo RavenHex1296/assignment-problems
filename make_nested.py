06ac3679e09b4b3cc1a6625a6a340c5e30b6b98a
Cayden Lau <suanoo5196@gmail.com>
2020-08-25 23:01:43 +0000

    completed 3-2

---
+++ make_nested.py
@@ -0,0 +1,34 @@
+
+def make_nested(input_dict):
+    final_dict = {}
+
+    for key in input_dict:
+        splitter = key.split("_")
+        if splitter[0] not in final_dict:
+            final_dict[splitter[0]] = {}
+        final_dict[splitter[0]][splitter[1]] = input_dict.get(key)
+    return final_dict
+
+colors = {
+  'animal_bumblebee': ['yellow', 'black'],
+  'animal_elephant': ['gray'],
+  'animal_fox': ['orange', 'white'],
+  'food_apple': ['red', 'green', 'yellow'],
+  'food_cheese': ['white', 'orange']
+}
+
+print(make_nested(colors))
+
+print("Testing make_nested on the input_dict 'colors'")
+assert make_nested(colors) == {
+  'animal': {
+    'bumblebee': ['yellow', 'black'],
+    'elephant': ['gray'],
+    'fox': ['orange', 'white']
+  },
+  'food': {
+    'apple': ['red', 'green', 'yellow'],
+    'cheese': ['white', 'orange']
+  }
+}, "Incorrect nest"
+print("PASSED")
