48c6c8cb29ae01b89ba92698157250939e993676
Cayden Lau <suanoo5196@gmail.com>
2020-08-26 03:53:46 +0000

    fixed assert in count characters

--- count_characters.py
+++ count_characters.py
@@ -6,7 +6,8 @@ def count_characters(input_string):
         final_dictionary[character] = lowercase.count(character)
 
     return final_dictionary
-
+print(count_characters("A cat!!!"))
 print("Asserting count_characters works on input 'A cat!!!'")
-assert count_characters("A cat"), "Code is busted"
+assert count_characters("A cat") == {
+    'a': 2, ' ': 1, 'c': 1, 't': 1, '!': 3}, "Code isn't working properly"
 print("PASSED")
--- stack.py
+++ stack.py
@@ -1,1 +1,40 @@
-def stack():
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, new_data):
+        self.data.append(new_data)
+
+    def pop(self):
+        self.data.remove(self.data[len(self.data)-1])
+
+    def peek(self):
+        return self.data[len(self.data)-1]
+
+s = Stack()
+
+print("Asserting s.data")
+assert s.data == []
+print('PASSED')
+
+s.push('a')
+s.push('b')
+s.push('c')
+
+print("Asserting s.push")
+assert s.data == ['a', 'b', 'c']
+print('PASSED')
+
+s.pop()
+
+print("Asserting s.pop")
+assert s.data == ['a', 'b']
+print('PASSED')
+
+print("Asserting s.peek")
+assert s.peek() == 'b'
+print('PASSED')
+
+print("testing s.data...")
+assert s.data == ['a', 'b']
+print('PASSED')
