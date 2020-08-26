c4debcdaf7672326b54ee1a1087acbc66d3c658c
Cayden Lau <suanoo5196@gmail.com>
2020-08-26 05:10:23 +0000

    fixed union_intersection

--- union_intersection.py
+++ union_intersection.py
@@ -2,7 +2,7 @@ def intersection(list_1, list_2):
     return list(set(list_1) & set(list_2))
 
 print("Asserting intersection on inputs [1, 2, 'a', 'b'], [2, 3, 'a']")
-assert intersection([1, 2, 'a', 'b'], [2, 3, 'a']) == [2, 'a'], "Incorrect intersection"
+assert intersection([1, 2, "a", "b"], [2, 3, "a"]) == [2, 'a'] or ['a', 2], "Incorrect intersection"
 print("PASSED")
 
 
@@ -10,5 +10,5 @@ def union(list_1, list_2):
     return list(set(list_1) | set(list_2))
 
 print("Asserting union on inputs [1, 2, 'a', 'b'], [2, 3,'a']")
-assert union([1, 2, 'a', 'b'], [2, 3, 'a']) == [1, 2, 3, 'a', 'b'], "Incorrect union"
+assert union([1, 2, "a", "b"], [2, 3, "a"]) == [1, 2, 3, 'a', 'b'] or [1, 2, 'a', 3, 'b'], "Incorrect union"
 print("PASSED")
