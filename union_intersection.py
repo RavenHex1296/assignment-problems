d442bd50312e78ef0f0ad05f47bd70f1282d1394
Cayden Lau <suanoo5196@gmail.com>
2020-08-26 04:44:54 +0000

    made some lines shorter by line break

--- union_intersection.py
+++ union_intersection.py
@@ -1,10 +1,18 @@
 def intersection(list_1, list_2):
     return list(set(list_1) & set(list_2))
 
-print(intersection([1, 2, 'a', 'b'], [2, 3, 'a']))
+print("Asserting intersection on inputs [1, 2, 'a', 'b'], [2, 3, 'a']")
+assert intersection([1, 2, 'a', 'b'], [2, 3,
+                                       'a']) == [2,
+                                                 'a'], "Incorrect intersection"
+print("PASSED")
 
 
 def union(list_1, list_2):
     return list(set(list_1) | set(list_2))
 
-print(union([1, 2, 'a', 'b'], [2, 3, 'a']))
+print("Asserting union on inputs [1,2,'a','b'], [2,3,'a']...")
+assert union([1, 2, 'a', 'b'], [2, 3,
+                                'a']) == [1,
+                                          2, 3, 'a', 'b'], "Incorrect union"
+print("PASSED")
