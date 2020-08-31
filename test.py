3baa4bb42fea8f6d19d50cf0235c289bf7448ae2
Cayden Lau <suanoo5196@gmail.com>
2020-08-31 04:13:57 +0000

    fixed 4-3(b)

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ def decode(numbers, a, b):
     decoded_string = ""
     for element in numbers:
         decoded_string += letters[int((element - b) / a)]
-        if ((element - b) / a).is_integer() == False or int((element - b) / a) < 0 or letters[int((element - b) / a)] == "z":
-          return False
+        if ((element - b) / a).is_integer() == False or int((element - b) / a) < 0 or int((element - b) / a) >25:
+            return False
     return decoded_string
+
+print(decode([5, 3, 9, 5, 44], 2, 3))
