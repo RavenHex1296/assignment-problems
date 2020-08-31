b7d7e786dfdc59c60daab129b1d6568433f81e00
Cayden Lau <suanoo5196@gmail.com>
2020-08-31 04:47:30 +0000

    started part c on problem 4-3, didnt get far, couldnt figure out how to take all possible combinations within a range

---
+++ h.py
@@ -0,0 +1,3 @@
+x = range(1,101)
+for n in x:
+  print(n)
--- test.py
+++ test.py
@@ -1,12 +1,37 @@
 letters = " abcdefghijklmnopqrstuvwxyz"
 
 
-def decode(numbers, a, b):
+def decode(numbers):
     decoded_string = ""
     for element in numbers:
         decoded_string += letters[int((element - b) / a)]
-        if ((element - b) / a).is_integer() == False or int((element - b) / a) < 0 or int((element - b) / a) >25:
-            return False
+
     return decoded_string
 
-print(decode([5, 3, 9, 5, 44], 2, 3))
+print(decode([377,
+              717,
+              71,
+              513,
+              105,
+              921,
+              581,
+              547,
+              547,
+              105,
+              377,
+              717,
+              241,
+              71,
+              105,
+              547,
+              71,
+              377,
+              547,
+              717,
+              751,
+              683,
+              785,
+              513,
+              241,
+              547,
+              751]))
