3db55e18e3e08f4fd004f50044c547a96d46f8a5
Cayden Lau <suanoo5196@gmail.com>
2020-09-02 21:15:24 +0000

    finished 5-3

--- queue.py
+++ queue.py
@@ -6,24 +6,34 @@ class Queue:
         self.data.append(item)
 
     def dequeue(self):
-        return self.data.popleft()
+        return self.data.pop(0)
     
     def peek(self):
         return self.data[0]
 
 q = Queue()
 
-print(q.data)
+print("Asserting q.data")
+assert q.data == [], "Incorrect output"
+print("PASSED")
 
 q.enqueue('a')
 q.enqueue('b')
 q.enqueue('c')
-print(q.data)
+print("Asserting q.enqueue()")
+assert q.data == ["a", "b", "c"], "Incorrect output"
+print("PASSED")
 
 q.dequeue()
-print(q.data)
+print("Asserting q.dequeue()")
+assert q.data == ["b", "c"], "Incorrect output"
+print("PASSED")
 
 q.peek()
-print(q.peek())
+print("Asserting q.peek()")
+assert q.peek() == "b", "Incorrect output"
+print("PASSED")
 
-print(q.data)
+print("Asserting q.data")
+assert q.data == ["b", "c"]
+print("PASSED")
