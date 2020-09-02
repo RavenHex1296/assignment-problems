c37c12097fff0cb3f67646e36f9ba0da87214cba
Cayden Lau <suanoo5196@gmail.com>
2020-09-02 15:04:20 +0000

    changed base_1_input to base_10 input

--- convert_to_base_2.py
+++ convert_to_base_2.py
@@ -1,12 +1,12 @@
-def convert_to_base_2(base_1_input):
+def convert_to_base_2(base_10_input):
     binary_number = [0] * base_1_input
     index = 0
     base_2_string = ""
 
-    while (base_1_input > 0):
+    while (base_10_input > 0):
 
         binary_number[index] = base_1_input % 2
-        base_1_input = int(base_1_input / 2)
+        base_10_input = int(base_1_input / 2)
         index += 1
 
     for string_index in range(index - 1, -1, -1):
