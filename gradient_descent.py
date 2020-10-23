7a3b7e32b7999dddec23b2305526d4e19d384362
Cayden Lau <suanoo5196@gmail.com>
2020-10-23 00:15:29 +0000

    changed g(x) to f2(x) in gradient_descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -5,8 +5,8 @@ def f(x):
     return x ** 2 + 2 * x + 1
 
 
-def g(x):
-    return (x**2 + math.cos(x)) / math.exp(math.sin(x))
+def f2(x):
+    return (x ** 2 + math.cos(x)) / math.exp(math.sin(x))
 
 
 def gradient_descent(f, x0, alpha, delta, iterations=1000):
@@ -21,4 +21,4 @@ def gradient_descent(f, x0, alpha, delta, iterations=1000):
 
 print("Estimated minimum value for x^2+2x+1: " + str(gradient_descent(f, 0, 0.01, 0.0001, 10000)))
 
-print("Estimated minimum value for (x^2+cos(x))/(e^sin(x)): " + str(gradient_descent(g, 0, 0.01, 0.0001, 10000)))
+print("Estimated minimum value for (x^2+cos(x))/(e^sin(x)): " + str(gradient_descent(f2, 0, 0.01, 0.0001, 10000)))
