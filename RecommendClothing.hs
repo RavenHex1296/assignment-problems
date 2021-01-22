33e448ed58516ca87bad45fefb41d4970af90e0e
Cayden Lau <suanoo5196@gmail.com>
2021-01-22 04:29:12 +0000

    finished RecommendClothing.hs

---
+++ RecommendClothing.hs
@@ -0,0 +1,9 @@
+recommendClothing :: (RealFloat a) => a -> String  
+recommendClothing degreesCelsius  
+    | degreesFahrenheit  >= 80 = "Wear a shortsleeve shirt"
+    | degreesFahrenheit  > 65 = "Wear a longsleeve shirt"
+    | degreesFahrenheit  > 50 = "Wear a sweater"
+    | degreesFahrenheit  <= 50 = "Wear a jacket"
+    where degreesFahrenheit  = degreesCelsius * 9/5 + 32 
+
+main = print (recommendClothing 5)
